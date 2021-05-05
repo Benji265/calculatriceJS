98a0a1ccbc545bc1f9ab5525342be605f90743ec
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-05-05 16:54:14 +0200

    Ajout des premiere etape

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,19 @@
-const dataNumber = document.querySelectorAll("div[data-number='number']");
+const allButtonNumber = document.querySelectorAll("button[data-number]");
+const allButtonOperator = document.querySelectorAll("button[data-operator]");
 
-dataNumber.forEach(element => {
-     element.addEventListener("click", function(){
-         console.log(this)
-     })
- });
+//On parcours le tableaux des nombre
+allButtonNumber.forEach(element => {
+    //On cree une fonction qui permet d'afficher les chiffres dans l'id numberOne
+    element.addEventListener("click", function(){
+        numberOne.innerHTML += this.dataset.number;
+    })
+});
+
+//On parcours le tableaux des operateur
+allButtonOperator.forEach(element => {
+    //On cree une fonction qui permet d'afficher les operateur dans l'id operator
+    element.addEventListener("click", function(){
+        operator.innerHTML = this.dataset.operator;
+    })
+});
+
