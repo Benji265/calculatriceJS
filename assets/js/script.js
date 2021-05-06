207809efec7a3fc0f11b4bd4ae77557204d27eb8
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-05-06 10:21:56 +0200

    Fin de la partie JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const allButtonNumber = document.querySelectorAll("button[data-number]");
 const allButtonOperator = document.querySelectorAll("button[data-operator]");
 const resultCalc = document.querySelector("button[data-result]");
+const resetCalc = document.querySelector("button[data-reset]");
 
 
 //On parcours le tableaux des nombre
@@ -15,7 +16,7 @@ allButtonNumber.forEach(element => {
             numberOne.innerHTML += this.dataset.number;
             
         }
-    })
+    });
 });
 
 //On parcours le tableaux des operateur
@@ -23,10 +24,10 @@ allButtonOperator.forEach(element => {
     //On cree une fonction qui permet d'afficher les operateur dans l'id operator
     element.addEventListener("click", function(){
         operator.innerHTML = this.dataset.operator;
-    })
+    });
 });
 
-
+//Ajout d'un siwtch qui parcours pour savoir quelle operateur est selectionner pour faire l'operation prevus
 resultCalc.addEventListener("click", function(){
     switch (operator.innerHTML) {
         case "+":
@@ -48,4 +49,12 @@ resultCalc.addEventListener("click", function(){
             "Veuillez mettre un opérateur";
             break;
     }
-})
+});
+
+//On cree une fonction qui permet d'enlever les chiffres dans chaque div
+resetCalc.addEventListener("click", function(){
+    numberOne.innerHTML = "";
+    numberTwo.innerHTML = "";
+    result.innerHTML = "";
+    operator.innerHTML = "";
+});
