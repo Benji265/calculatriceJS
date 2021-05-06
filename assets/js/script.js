3b39d34603ce8ebb478561fea3f7509eb7fc1cfd
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-05-06 09:39:56 +0200

    Ajout du deuxieme nombre dans la fonction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 const allButtonNumber = document.querySelectorAll("button[data-number]");
 const allButtonOperator = document.querySelectorAll("button[data-operator]");
+const test = document.getElementById("operator")
+
+console.log(operator);
 
 //On parcours le tableaux des nombre
 allButtonNumber.forEach(element => {
-    //On cree une fonction qui permet d'afficher les chiffres dans l'id numberOne
+    //On cree une fonction qui permet d'afficher les chiffres dans les div
     element.addEventListener("click", function(){
-        numberOne.innerHTML += this.dataset.number;
+        //Si la div operateur  est vide on ecrit dans numberOne
+        if(operator.innerHTML != ""){
+            numberTwo.innerHTML += this.dataset.number;
+            //Si on a cliquer sur un operateur on ecrit dans numberTwo
+        } else {
+            numberOne.innerHTML += this.dataset.number;
+            
+        }
     })
 });
 
